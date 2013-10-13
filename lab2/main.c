25b029ae485850369f496d6a8e9ce1b291193cb3
darkscream99 <bloodprofag@mail.ru>
2013-10-14 01:33:02 +0300

    Indent lab2 main.c

--- lab2/main.c
+++ lab2/main.c
@@ -2,52 +2,46 @@
 
 char *program_name;
 
-static void server( SOCKET s, struct sockaddr_in *peerp )
+static void server(SOCKET s, struct sockaddr_in *peerp)
 {
-	int rc;
-	char buf[ 120 ];
+    int rc;
+    char buf[120];
 
-	rc = recv( s, buf, sizeof( buf ), 0 );
-	send( s, buf, rc, 0);
+    rc = recv(s, buf, sizeof(buf), 0);
+    send(s, buf, rc, 0);
 }
 
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-	struct sockaddr_in peer;
-	char *hname;
-	char *sname;
-	int peerlen;
-	SOCKET s1;
-	SOCKET s;
-	const int on = 1;
+    struct sockaddr_in peer;
+    char *hname;
+    char *sname;
+    int peerlen;
+    SOCKET s1;
+    SOCKET s;
+    const int on = 1;
 
-	INIT();
+    INIT();
 
-	if ( argc == 2 )
-	{
-		hname = NULL;
-		sname = argv[ 1 ];
-	}
-	else if ( argc == 3 )
-	{
-		hname = argv[ 1 ];
-		sname = argv[ 2 ];
-	}
-	else
-	{
-		error( 1, errno, "count of parametrs mismatch\n" );
-	}
+    if (argc == 2) {
+        hname = NULL;
+        sname = argv[1];
+    } else if (argc == 3) {
+        hname = argv[1];
+        sname = argv[2];
+    } else {
+        error(1, errno, "count of parametrs mismatch\n");
+    }
 
-	s = tcp_server( hname, sname );
+    s = tcp_server(hname, sname);
 
-	do
-	{
-		peerlen = sizeof( peer );
-		s1 = accept( s, ( struct sockaddr * )&peer, &peerlen );
-		if ( !isvalidsock( s1 ) )
-			error( 1, errno, "error function accept\n" );
-		server( s1, &peer );
-		CLOSE( s1 );
-	} while ( 1 );
-	EXIT( 0 );
+    do {
+        peerlen = sizeof(peer);
+        s1 = accept(s, (struct sockaddr *) &peer, &peerlen);
+        if (!isvalidsock(s1))
+            error(1, errno, "error function accept\n");
+        server(s1, &peer);
+        CLOSE(s1);
+    } while (1);
+    EXIT(0);
 }
